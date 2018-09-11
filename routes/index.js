c3bb665d9dad4802540cf756608c7f036f5d971b
Nick <nicholasjohnrichardson@gmail.com>
2018-09-11 18:16:44 +0100

    added some styling

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 var Sonnet = require('../public/javascripts/sonnet.js');
 
-const sonnet = new Sonnet();
-
-/* GET home page. */
 router.get('/', function(req, res, next) {
+  const sonnet = new Sonnet();
   sonnet.compose().then((sonnet) => {
     res.render('index', { title: 'Sonneteer', sonnet: sonnet });
   })
